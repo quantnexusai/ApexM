508476313f2ffd838d75c58954a4c13d12a809f5
aritellavsn <153779058+quantnexusai@users.noreply.github.com>
2024-12-23 11:42:29 -0800

    christmas_update

--- packages/components/credentials/XaiApi.credential.ts
+++ packages/components/credentials/XaiApi.credential.ts
@@ -11,8 +11,6 @@ class XaiApi implements INodeCredential {
         this.label = 'Xai API'
         this.name = 'xaiApi'
         this.version = 1.0
-        this.description =
-            'Refer to <a target="_blank" href="https://docs.voyageai.com/install/#authentication-with-api-keys">official guide</a> on how to get an API Key'
         this.inputs = [
             {
                 label: 'X AI API Key',
