017419b6a961e1bd5d1ee5ab83b5ea04e0d57947
aritellavsn <153779058+quantnexusai@users.noreply.github.com>
2025-01-05 06:20:51 -0800

    json_error

--- packages/components/credentials/SerperApi.credential.ts
+++ packages/components/credentials/SerperApi.credential.ts
@@ -11,6 +11,8 @@ class SerperApi implements INodeCredential {
         this.label = 'Serper API'
         this.name = 'serperApi'
         this.version = 1.0
+        this.description =
+            'Refer to <a target="_blank" href="https://serpapi.com/search-api">official guide</a> on how to create an API key for Serp API'
         this.inputs = [
             {
                 label: 'Serper Api Key',
--- packages/components/credentials/TogetherAIApi.credential.ts
+++ packages/components/credentials/TogetherAIApi.credential.ts
@@ -4,13 +4,14 @@ class TogetherAIApi implements INodeCredential {
     label: string
     name: string
     version: number
+    description: string
     inputs: INodeParams[]
 
     constructor() {
         this.label = 'TogetherAI API'
         this.name = 'togetherAIApi'
         this.version = 1.0
-        this.description =
+       this.description =
             'Refer to <a target="_blank" href="https://docs.together.ai/reference/authentication-1">official guide</a> on how to create an API key for Together AI'
         this.inputs = [
             {
